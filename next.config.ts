16c74deb424ab7c228020debb28fdd3b707b401c
Tara Khanal <tarakh99@gmail.com>
2026-04-19 04:40:34 -0400

    Disable server side rendering

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
   allowedDevOrigins: ["http://localhost:3000", "192.168.1.14"],
 };
 
