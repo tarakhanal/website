e847c891da76019169d0080c96d091c4d1b06c9e
Tara Khanal <tarakh99@gmail.com>
2026-09-16 06:51:41 -0400

    fix failing build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   trailingSlash: true,
   images: {
-    unoptimized: false,
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
